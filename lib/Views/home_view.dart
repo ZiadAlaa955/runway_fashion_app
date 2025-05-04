6261feb63476caeabb2475dbda416fbe6f9bcd24
Ziad Alaa <ziadalaa955@gmail.com>
2025-05-04 17:27:00 +0300

    create categories list view and bottom navigation bar

--- lib/Views/home_view.dart
+++ lib/Views/home_view.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:gap/gap.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:runway_fashion_app/Widgets/categories_list_view.dart';
 import 'package:runway_fashion_app/Widgets/custom_app_bar.dart';
 import 'package:runway_fashion_app/Widgets/home_video.dart';
 
@@ -15,20 +15,34 @@ class HomeView extends StatelessWidget {
         prefixIcon: 'assets/images/icons/menu.svg',
         suffixIcon: 'assets/images/icons/notification.svg',
       ),
-      body: Column(
+      body: const Column(
         crossAxisAlignment: CrossAxisAlignment.start,
         children: [
-          const HomeVideo(),
-          const Gap(10),
-          Text(
-            'Categories',
-            style: TextStyle(
-              fontSize: 16,
-              fontWeight: FontWeight.w600,
-              fontFamily: GoogleFonts.inter().fontFamily,
-            ),
+          HomeVideo(),
+          Gap(10),
+          CategoriesListView(),
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        selectedItemColor: Colors.black,
+        type: BottomNavigationBarType.fixed,
+        items: const [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
           ),
-          const Gap(10),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.favorite),
+            label: 'Likes',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.shopping_bag),
+            label: 'Bag',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.person),
+            label: 'Profile',
+          ),
         ],
       ),
     );
---
+++ lib/Widgets/categories_list_view.dart
@@ -0,0 +1,83 @@
+import 'package:flutter/material.dart';
+import 'package:gap/gap.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:runway_fashion_app/Models/category_model.dart';
+
+class CategoriesListView extends StatelessWidget {
+  const CategoriesListView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    List<CategoryModel> categories = [
+      CategoryModel(
+        title: 'Women',
+        image: 'assets/images/categories/women.png',
+      ),
+      CategoryModel(
+        title: 'Men',
+        image: 'assets/images/categories/Men.png',
+      ),
+      CategoryModel(
+        title: 'Kids',
+        image: 'assets/images/categories/kids.png',
+      ),
+      CategoryModel(
+        title: 'Deals',
+        image: 'assets/images/categories/Deals.png',
+      ),
+      CategoryModel(
+        title: 'Home',
+        image: 'assets/images/categories/Home.png',
+      ),
+    ];
+
+    return Padding(
+      padding: const EdgeInsets.only(left: 15),
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            'Categories',
+            style: TextStyle(
+              fontSize: 18,
+              fontWeight: FontWeight.w900,
+              fontFamily: GoogleFonts.inter().fontFamily,
+            ),
+          ),
+          const Gap(20),
+          SizedBox(
+            height: 110,
+            child: ListView.builder(
+              scrollDirection: Axis.horizontal,
+              itemCount: categories.length,
+              itemBuilder: (context, index) {
+                return Padding(
+                  padding: const EdgeInsets.only(right: 10),
+                  child: Column(
+                    children: [
+                      CircleAvatar(
+                        radius: 40,
+                        child: Image.asset(
+                          categories[index].image,
+                        ),
+                      ),
+                      const Gap(5),
+                      Text(
+                        categories[index].title,
+                        style: TextStyle(
+                          fontSize: 16,
+                          fontWeight: FontWeight.w600,
+                          fontFamily: GoogleFonts.inter().fontFamily,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              },
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
